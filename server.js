1f369117058ac19806f5ad95dbc6763f545ea7db
Juniorlk <juniorlekeu@gmail.com>
2024-05-19 14:55:05 +0100

    2 commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
@@ -10,38 +10,15 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://juniorlk:juniorlk@clusterjamboinfo.5pwdk22.mongodb.net/?retryWrites=true&w=majority&appName=ClusterJamboInfo";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect('mongodb+srv://juniorlk:juniorlk@clusterjamboinfo.5pwdk22.mongodb.net/?retryWrites=true&w=majority&appName=ClusterJamboInfo', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
-app.listen(port, () => {
-    console.log(`Serveur démarré sur le port ${port}`);
-   });
-   
-// Routes
+// // Routes
 // const studentsRouter = require('./routes/students');
 // app.use('/students', studentsRouter);
 
